fa9718c6c72e06e845c994f5e962552f78ba9bd6
tongfy <573682532@qq.com>
2019-01-03 15:42:10 +0800

    Demo仓库提交 ——————————————————————— *

--- PlayerDemo/PlayerDemo/TTAudioPlayer/TTPlayerProtocol.h
+++ PlayerDemo/PlayerDemo/TTAudioPlayer/TTPlayerProtocol.h
@@ -11,34 +11,34 @@
 
 @protocol TTPlayerProtocol <NSObject>
 
+////// 播放歌曲
 - (void) playMusicWithInfo:(id<TTMusicModelProtocol>)model;
 
+#pragma mark - 播放队列控制
+//// 将歌曲添加播放列表列尾
 - (void) enterPlayQueueWithSingle:(id<TTMusicModelProtocol>)model;
+//// 将数组歌曲添加播放列表列尾
 - (void) enterPlayQueueWithArray:(NSArray<id<TTMusicModelProtocol>> *)models;
 
+///// 清空播放列表，并将当前歌曲加入播放列表
 - (void) replaceAllQueueWithSingle:(id<TTMusicModelProtocol>)model;
+///// 清空播放列表，并将当前歌曲数组加入播放列表
 - (void) replaceAllQueueWithArray:(NSArray<id<TTMusicModelProtocol>> *)models;
 
-- (BOOL) isPlaying;
 
-- (BOOL) isNextExist;
-
-- (BOOL) isPreExist;
-
+#pragma mark - 播放控制
 - (void) next;
-
 - (void) pre;
-
 - (void) pause;
-
 - (void) stop;
-
 - (void) play;
-
 - (void) continuePlay;
 
+#pragma mark - 播放信息
 - (double) currentSecs;
-
 - (double) durationSecs;
+- (BOOL) isPlaying;
+- (BOOL) isNextExist;
+- (BOOL) isPreExist;
 
 @end
--- PlayerDemo/PlayerDemo/ViewController.m
+++ PlayerDemo/PlayerDemo/ViewController.m
@@ -28,32 +28,29 @@
         make.size.mas_equalTo(playBar.bounds.size);
     }];
     
-    
-//    NSString *config = @"[{\"audioUrl\":\"http://tmjl128.alicdn.com/194/7194/2102408807/1792653269_1476762863432.mp3?auth_key=1543978800-0-0-d41777b7e924ab71959ae54af6adc6e3\",\"audioName\":\"数鸭子\",\"audioAnchor\":\"李志\"},{\"audioUrl\":\"http://tmjl128.alicdn.com/428/62428/422853/1770001654_1512634063795.mp3?auth_key=1543978800-0-0-70a986c9328714721c0f5381e2b7b514\",\"audioName\":\"幸福的孩子爱唱歌\",\"audioAnchor\":\"谢欣芷\"},{\"audioUrl\":\"http://tmjl128.alicdn.com/147/7147/298010/3302004_1532947360257.mp3?auth_key=1543978800-0-0-03c7c35a7d746545ba33c085158d48b7\",\"audioName\":\"嘟嘟歌\",\"audioAnchor\":\"黄建为\"},{\"audioUrl\":\"http://tmjl128.alicdn.com/841/395312841/695312853/1772798197_15662156_l.mp3?auth_key=1543978800-0-0-bac3b7dd08f7fb9642e7820940448f41\",\"audioName\":\"我有一只小毛驴\",\"audioAnchor\":\"碧瑶\"},]";
-    
     TTMusicModel *item1 = [TTMusicModel new];
     item1.name = @"数鸭子";
-    item1.url  = @"http://tmjl128.alicdn.com/135/135/2103675237/1802873715_1523364273251.mp3?auth_key=1546484400-0-0-60cec68686116750cfe45abcdb69dac5";
+    item1.url  = @"http://tmjl128.alicdn.com/158/7158/1136455538/1774490672_1478671781016.mp3?auth_key=1547089200-0-0-716d40c725d7be545e657ee1a16e7c5a";
     item1.author = @"李志";
     
     TTMusicModel *item2 = [TTMusicModel new];
     item2.name = @"幸福的孩子爱唱歌";
-    item2.url  = @"http://tmjl128.alicdn.com/135/135/2103675237/1802873715_1523364273251.mp3?auth_key=1546484400-0-0-60cec68686116750cfe45abcdb69dac5";
+    item2.url  = @"http://tmjl128.alicdn.com/158/7158/1136455538/1774490672_1478671781016.mp3?auth_key=1547089200-0-0-716d40c725d7be545e657ee1a16e7c5a";
     item2.author = @"谢欣芷";
     
     TTMusicModel *item3 = [TTMusicModel new];
     item3.name = @"我有一只小毛驴";
-    item3.url  = @"http://tmjl128.alicdn.com/135/135/2103675237/1802873715_1523364273251.mp3?auth_key=1546484400-0-0-60cec68686116750cfe45abcdb69dac5";
+    item3.url  = @"http://tmjl128.alicdn.com/158/7158/1136455538/1774490672_1478671781016.mp3?auth_key=1547089200-0-0-716d40c725d7be545e657ee1a16e7c5a";
     item3.author = @"碧瑶";
     
     TTMusicModel *item4 = [TTMusicModel new];
     item4.name = @"我有一只小毛驴";
-    item4.url  = @"http://tmjl128.alicdn.com/135/135/2103675237/1802873715_1523364273251.mp3?auth_key=1546484400-0-0-60cec68686116750cfe45abcdb69dac5";
+    item4.url  = @"http://tmjl128.alicdn.com/158/7158/1136455538/1774490672_1478671781016.mp3?auth_key=1547089200-0-0-716d40c725d7be545e657ee1a16e7c5a";
     item4.author = @"碧瑶";
     
     TTMusicModel *item5 = [TTMusicModel new];
     item5.name = @"我有一只小毛驴";
-    item5.url  = @"http://tmjl128.alicdn.com/135/135/2103675237/1802873715_1523364273251.mp3?auth_key=1546484400-0-0-60cec68686116750cfe45abcdb69dac5";
+    item5.url  = @"http://tmjl128.alicdn.com/158/7158/1136455538/1774490672_1478671781016.mp3?auth_key=1547089200-0-0-716d40c725d7be545e657ee1a16e7c5a";
     item5.author = @"碧瑶";
     
     NSArray *items = @[item1,item2,item3,item4,item5];
@@ -63,39 +60,5 @@
     
     self.view.backgroundColor = [UIColor colorWithRed:255/255.0 green:0/255.0 blue:0/255.0 alpha:1.0];
 }
-
-//- (void)didReceiveMemoryWarning {
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
-//
-//- (void)ttAudioPlayerPlayStart{
-//    [[AudioPlayBar sharePlayBar] updatePlayStatus:YES];
-//}
-//- (void)ttAudioPlayerPlayFinished{
-//    [[AudioPlayBar sharePlayBar] updatePlayStatus:NO];
-//}
-//- (void)ttAudioPlayerPause{
-//    [[AudioPlayBar sharePlayBar] updatePlayStatus:NO];
-//}
-//- (void)ttAudioPlayerStoped{
-//    [[AudioPlayBar sharePlayBar] updatePlayStatus:NO];
-//}
-//- (void)ttAudioPlayerPlayError:(NSError *)error{
-//    [[AudioPlayBar sharePlayBar] updatePlayStatus:NO];
-//    NSLog(@"音乐播放错误%@",error.description);
-//}
-//- (void)ttAudioPlayerSeekPosition:(double)progress{
-//    NSLog(@"");
-//}
-//- (void)ttAudioPlayerUpdateProgress:(double)progress
-//{
-//    [[AudioPlayBar sharePlayBar] updateProgress:progress];
-//}
-//
-//- (void)ttAduioPlayerMusicInfoUpdate:(id<TTMusicModelProtocol>)musicInfo
-//{
-//    [[AudioPlayBar sharePlayBar] updateMusicInfo:musicInfo];
-//}
 
 @end
